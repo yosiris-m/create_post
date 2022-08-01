450dcb1db0e56c8c03c2eaea837892fbb3139fb9
[yosiris] <[yosiris0510@gmail.com]>
2022-08-01 20:53:00 +0200

    create react app with react router

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Travel Blog</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
